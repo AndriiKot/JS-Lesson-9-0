fd3aeeab3d1479ea80e094d8832d9175f0ef3bcd
AndriiKot <AndriiKot@users.noreply.github.com>
2025-04-29 17:41:39 +0200

    feate: add useRounter to not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,22 @@
+'use client';
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/");
+    }, 3000);
+  });
   return (
     <>
-      <div className={ styles["not-found"] }>
+      <div className={styles["not-found"]}>
         <h1>404 - Page Not Found</h1>
         <p>Sorry, but the requested page does not exist.</p>
         <p>
