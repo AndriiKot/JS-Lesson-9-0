1520a3efd2cf73eb7dd3323851de503fb27bb5dc
AndriiKot <AndriiKot@users.noreply.github.com>
2025-04-29 13:15:09 +0200

    feate: add css style to not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
+import styles from "./NotFoundPage.module.css";
 
-
-const NotFoundPage = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    <div className='not-found'>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, but the requested page does not exist.</p>
-      <p>
-        Go back to the <Link href="/">home page</Link>.
-      </p>
-    </div>
+    <>
+      <div className={ styles["not-found"] }>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, but the requested page does not exist.</p>
+        <p>
+          Go back to the <Link href="/">home page</Link>.
+        </p>
+      </div>
+    </>
   );
 };
 
